78e5a7e971b68c41e17729229aad2a1d37505842
Lucas Edwards <lucas.w.edwards@gmail.com>
2018-10-30 15:41:40 -0400

    Wwise Added
    Integrated Wwise. Empty Soundbank created and initialized on start. Soundbanks generated for WINDOWS platform.

---
+++ Assets/Scripts/Audio Scripts/BankLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BankLoader : MonoBehaviour {
+
+    public AK.Wwise.Bank GameSoundbank;
+
+	void Start ()
+    {
+        GameSoundbank.Load();
+	}
+	
+
+}
