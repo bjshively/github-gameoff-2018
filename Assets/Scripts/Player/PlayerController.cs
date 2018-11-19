910b4bd899aeb2a07a42bcd20fb5f0afad57826a
Bradley Shively <bshively@gmail.com>
2018-11-18 23:56:55 -0500

    Basic controls implementation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody body;
+    private Animator anim;
 
 
     private float horizontal;
     private float vertical;
     public IntVariable health;
     public IntVariable maxHealth;
+    public FloatVariable moveSpeed;
 
     // Use this for initialization
     void Start () {
         health.Value = maxHealth.Value;
         body = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 
 	}
 	
@@ -25,8 +28,12 @@ public class PlayerController : MonoBehaviour {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         Move(horizontal, vertical);
-        Debug.Log(health.Value);
 
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Punch();
+        }
 
         if (health.Value <= 0)
         {
@@ -41,14 +48,37 @@ public class PlayerController : MonoBehaviour {
 
     private void Move(float h, float v)
     {
+        //if (h > 0)
+        //{
+        //    h = moveSpeed.Value;
+        //} else if (h < 0)
+        //{
+        //    h = moveSpeed.Value * -1f;
+        //}
+
+        //if (v > 0)
+        //{
+        //    v = moveSpeed.Value;
+        //}
+        //else if (h < 0)
+        //{
+        //    v = moveSpeed.Value * -1f;
+        //}
+
         body.velocity = new Vector3(h, 0.0f, v).normalized;
         
         //body.velocity = new Vector2(moveSpeed * Mathf.Sign(h), body.velocity.y);
         //transform.position += moveNormal * Time.deltaTime * MoveRate.Value;
     }
 
+    private void Punch()
+    {
+        anim.SetTrigger("Punch");
+        Debug.Log("Punch");
+    }
+
     private void Die()
     {
-        Debug.Log("Dead");
+        // Debug.Log("Dead");
     }
 }
