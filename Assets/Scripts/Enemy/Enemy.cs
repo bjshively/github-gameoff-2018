99cb4991c0aec029d26efe2a29ac422b33135341
Bradley Shively <bshively@gmail.com>
2018-11-21 17:38:50 -0500

    Make enemy look at player when moving towards player

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    private Animator anim;
+    private Rigidbody body;
     public GameObject attackCollider;
     public TransformVariable playerTransform;
     public float playerActivationDistance = 20;
     float playerDistance;
-
+    public float moveSpeed = 5;
 
 	// Use this for initialization
 	void Start () {
-
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         playerDistance = Mathf.Abs(Vector3.Distance(playerTransform.Value.position, transform.position));
         Debug.Log(playerDistance);
         Move();
@@ -26,11 +29,19 @@ public class Enemy : MonoBehaviour {
     {
         if (playerDistance < playerActivationDistance)
         {
+            anim.SetFloat("MoveSpeed", 1);
             // The step size is equal to speed times frame time.
             float step = 5 * Time.deltaTime;
 
             // Move our position a step closer to the target.
             transform.position = Vector3.MoveTowards(transform.position, playerTransform.Value.position, step);
+
+            // Rotate to face player
+            transform.LookAt(playerTransform.Value.position);
+
+        } else
+        {
+            anim.SetFloat("MoveSpeed", 0);
         }
     }
 
