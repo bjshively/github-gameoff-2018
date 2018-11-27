6f4ed6d7ad6d2a654f5f21c12030f720eec3ae41
Bradley Shively <bshively@gmail.com>
2018-11-26 22:57:19 -0500

    Create EnemyActivator prefab

---
+++ Assets/Scripts/EnemyAwakener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAwakener : MonoBehaviour {
+
+    public GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            enemies[i].GetComponent<Enemy>().isAwake = false;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].GetComponent<Enemy>().isAwake = true;
+            }
+        }
+    }
+}
