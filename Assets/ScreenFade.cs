31d57e30d1e6cc943c72904e82296d5962a0b445
Bradley Shively <bshively@gmail.com>
2018-11-29 18:16:02 -0500

    Add script to turn the screen black and reload the scene

---
+++ Assets/ScreenFade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenFade : MonoBehaviour {
+    Material black;
+    Color color;
+    public Text youDied;
+
+    // Use this for initialization
+    void Start()
+    {
+        black = GetComponent<RawImage>().material;
+        GetComponent<RawImage>().material = null;
+
+    }
+	// Update is called once per frame
+	void Update () {
+    }
+
+
+    public void TurnScreenBlack()
+    {
+        GetComponent<RawImage>().material = black;
+        StartCoroutine(RestartScene());
+    }
+
+    IEnumerator RestartScene()
+    {
+        youDied.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+
+}
