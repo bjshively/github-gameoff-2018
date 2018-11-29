edaf301bca342708ebf176a5c5755d1c5582e82d
Bradley Shively <bshively@gmail.com>
2018-11-29 01:48:08 -0500

    Enhance Enemy Awakener to be Enemy Awakener and Arena. Create prefab

--- Assets/Scripts/EnemyAwakener.cs
+++ Assets/Scripts/EnemyAwakener.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class EnemyAwakener : MonoBehaviour {
 
-    public GameObject[] enemies;
+    public List<GameObject> enemies;
+    public bool isArena;
+    public GameObject[] walls;
+    CinemachineVirtualCamera cam;
+   
 
 	// Use this for initialization
 	void Start () {
         // Start all enemies in a sleep state
-        for (int i = 0; i < enemies.Length; i++)
+        for (int i = 0; i < enemies.Count; i++)
         {
             enemies[i].GetComponent<Enemy>().SetAwake(false);
         }
+        cam = GameObject.Find("Cinemachine").GetComponent<CinemachineVirtualCamera>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		for (int i = enemies.Count - 1; i >= 0; i--)
+        {
+            if(enemies[i].GetComponent<Enemy>().health <= 0)
+            {
+                enemies.Remove(enemies[i]);
+            }
+        }
+        if (isArena)
+        {
+            if (enemies.Count == 0)
+            {
+                // If all the enemies are dead, turn off the second wall and trigger, reset camera follow
+                walls[1].SetActive(false);
+                cam.Follow = GameObject.Find("Player").transform.Find("CameraFollow").transform;
+                gameObject.SetActive(false);
+            }
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
@@ -25,12 +47,24 @@ public class EnemyAwakener : MonoBehaviour {
         // When the player passes through the trigger, awaken all enemies
         if(other.name == "Player")
         {
-            for (int i = 0; i < enemies.Length; i++)
+            for (int i = 0; i < enemies.Count; i++)
             {
                 enemies[i].GetComponent<Enemy>().SetAwake(true);
             }
 
-            //Destroy(gameObject);
+            if(isArena)
+            {
+                ToggleWalls(true);
+                cam.Follow = transform;
+            }
+        }
+    }
+
+    void ToggleWalls(bool b)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            walls[i].SetActive(b);
         }
     }
 }
