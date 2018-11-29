8e68badff47f110ab78c4844b04fc885f1c0ddb7
Bradley Shively <bshively@gmail.com>
2018-11-29 03:56:29 -0500

    Create bullet prefab and script

---
+++ Assets/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    public float lifetime = 6f;
+    public float moveSpeed = .5f;
+    Vector3 target;
+    public TransformVariable playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        target = playerTransform.Value.position;
+	}
+	
+	void Update () {
+        lifetime -= Time.deltaTime;
+
+        // Bullets die once they reach their target or their lifetime has elapsed
+        if (lifetime <= 0 || Mathf.Abs(Vector3.Distance(transform.position, target)) <= 1)
+        {
+            Die();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed);
+	}
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Die();
+        }
+    }
+}
