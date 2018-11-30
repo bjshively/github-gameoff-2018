c995a4763ef99b6c9a3172cb0d8e15b3e0293330
Bradley Shively <bshively@gmail.com>
2018-11-30 03:16:12 -0500

    Create a simple ending credits scroll scene

---
+++ Assets/TextScroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScroller : MonoBehaviour {
+
+    public float scrollSpeed = 3;
+    Vector3 end; 
+	// Use this for initialization
+	void Start () {
+        end = new Vector3(transform.position.x, 2550, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, end, scrollSpeed);
+    }
+}
