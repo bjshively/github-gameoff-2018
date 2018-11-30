dfa88ad0c86ec46f7db69e19f08fa17d9bffa927
Bradley Shively <bshively@gmail.com>
2018-11-30 18:44:37 -0500

    Make arena focalPoint match playerZ for up/down look during arena fights. Create new ArenaTemplate prefab

---
+++ Assets/MatchPlayerZ.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchPlayerZ : MonoBehaviour {
+
+    public TransformVariable playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, playerTransform.Value.position.z);
+	}
+}
