5c20bb9bf6bb9a984380df28bd96f74d241a7d03
Bradley Shively <bshively@gmail.com>
2018-11-30 03:47:43 -0500

    Fix y-axis of bullets so that they don't shoot diagnonally downwards at player position

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,21 +5,20 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     public float lifetime = 6f;
     public float moveSpeed = .5f;
-    Vector3 target;
     Vector3 moveDir;
     public TransformVariable playerTransform;
 
 	// Use this for initialization
 	void Start () {
-        moveDir = (playerTransform.Value.position - transform.position).normalized;
-        target = new Vector3(playerTransform.Value.position.x +20, playerTransform.Value.position.y, playerTransform.Value.position.z + 20);
+        // Calculate movedir based on player X and Z, but bullet's Y to avoid Y axis shifting.
+        moveDir = (new Vector3(playerTransform.Value.position.x, transform.position.y, playerTransform.Value.position.z) - transform.position).normalized;
 	}
 	
 	void Update () {
         lifetime -= Time.deltaTime;
         transform.position += moveDir * moveSpeed;
 
-        // Bullets die once they reach their target or their lifetime has elapsed
+        // Bullets die once their lifetime has elapsed
         if (lifetime <= 0)
         {
             Die();
