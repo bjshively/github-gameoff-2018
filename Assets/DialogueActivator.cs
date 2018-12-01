e94ff845ac76331c187bb075458ae070af0c0bbf
Bradley Shively <bshively@gmail.com>
2018-11-30 22:53:24 -0500

    Incorporate 2dGameKit dialogue UI system. Create dialogue trigger prefab.

---
+++ Assets/DialogueActivator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueActivator : MonoBehaviour {
+    public DialogueCanvasController dialogueBox;
+    public string message;
+    public float displayTime = 3;
+    bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+        dialogueBox.GetComponent<DialogueCanvasController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+    public void ShowMessage(string text) {
+        dialogueBox.ActivateCanvasWithText(text);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player" && !triggered)
+        {
+            triggered = true;
+            ShowMessage(message);
+            StartCoroutine(HideDialogue());
+        }
+    }
+
+    IEnumerator HideDialogue()
+    {
+        yield return new WaitForSeconds(displayTime);
+        dialogueBox.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/UI/TranslatedPhrases.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    [CreateAssetMenu]
+    public class TranslatedPhrases : OriginalPhrases
+    {
+        public OriginalPhrases originalPhrases;
+    }
