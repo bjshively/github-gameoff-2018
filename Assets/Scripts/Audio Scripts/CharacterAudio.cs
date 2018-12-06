4e617a4aa7f6a0440bb9532777f922cfaa0b9a39
Eric Mofield <ericmofield@gmail.com>
2018-12-05 21:19:39 -0500

    Create audio scripts, EnvironmentAudio prefab, audio assets.

---
+++ Assets/Scripts/Audio Scripts/CharacterAudio.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudio : MonoBehaviour {
+
+    public WwiseEventVariable PlayerDamageSound;
+    public WwiseEventVariable PlayerDeathSound;
+    public WwiseEventVariable PlayerSwingSound;
+    public WwiseEventVariable FootStepSound;
+    public WwiseEventVariable EnemyDamagedSound;
+    public WwiseEventVariable EnemySwingSound;
+    public WwiseEventVariable EnemyDeathSound;
+    public WwiseEventVariable BulletSound;
+
+    public void PlayFootstepSound()
+    {
+        FootStepSound.Value.Post(gameObject);
+    }
+
+    public void PlaySwingSound()
+    {
+        PlayerSwingSound.Value.Post(gameObject);
+    }
+
+    public void PlayDeathSound()
+    {
+        EnemyDeathSound.Value.Post(gameObject);
+    }
+
+    public void PlayFallDownSound()
+    {
+        PlayerDamageSound.Value.Post(gameObject);
+    }
+
+    public void PlayPlayerDamagedSound()
+    {
+        PlayerDamageSound.Value.Post(gameObject);
+    }
+
+    public void PlayPlayerDeathSound()
+    {
+        AkSoundEngine.StopAll();
+        PlayerDeathSound.Value.Post(gameObject);
+    }
+
+    private void PlayBulletSound()
+    {
+        BulletSound.Value.Post(gameObject);
+    }
+}
---
+++ Assets/Scripts/Audio Scripts/EnvironmentAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentAudio : MonoBehaviour
+{
+    public GameObject SceneObject;
+    public WwiseEventVariable GameplayMusic;
+    public WwiseEventVariable GameplayAmbience;
+
+    void Start()
+    {
+        GameplayMusic.Value.Post(SceneObject);
+        GameplayAmbience.Value.Post(SceneObject);
+    }
+}
