1d52f453ef3dac440cb40009fea56f3098d535ab
Eric Mofield <ericmofield@gmail.com>
2018-12-06 03:01:59 -0500

    Polish main menu.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,32 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Menu : MonoBehaviour {
-
+public class Menu : MonoBehaviour
+{
     public AK.Wwise.Event MenuMusic;
     public AK.Wwise.Event StartTransition;
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+    { 
         MenuMusic.Post(gameObject);
-
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetButton("Start"))
-        {
-            StartGame();
-        }
-	}
-
     public void StartGame()
     {
         StartTransition.Post(gameObject);
-        SceneManager.LoadScene("MainLevelEmo");
     }
 
     public void QuitGame()
