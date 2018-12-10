bccfed4298461fb54e34a59d524a7f232f8d49a0
Eric Mofield <ericmofield@gmail.com>
2018-12-09 20:35:35 -0500

    Testing out some damage scripts and assets.

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Damageable : MonoBehaviour
+{
+    //[Serializable]
+    //public class HealthEvent : UnityEvent<Damageable>
+    //{ }
+
+    //[Serializable]
+    //public class DamageEvent : UnityEvent<DamageDealer, Damageable>
+    //{ }
+
+    //[Serializable]
+    //public class HealEvent : UnityEvent<int, Damageable>
+    //{ }
+
+    public UnityEvent OnHealthSet;
+    public UnityEvent OnTakeDamage;
+    public UnityEvent OnDie;
+    public UnityEvent OnGainHealth;
+    public IntVariable maxHealth;
+    public IntVariable publicHealth;
+    public int currentHealth;
+    public bool invincible = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = maxHealth.Value;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        publicHealth.Value = currentHealth;
+    }
+
+    //public void TakeDamage(DamageDealer damager, Damageable damageable)
+    //{
+    //    Debug.Log("TakeDamage"); 
+    //    if (!invincible)
+    //    {
+    //        currentHealth -= damager.DamageAmount.Value;
+    //        OnHealthSet.Invoke();
+    //    }
+
+    //    OnTakeDamage.Invoke(damager, this);
+
+    //    if (currentHealth <= 0)
+    //    {
+    //        OnDie.Invoke(damager, this);
+    //        invincible = true;
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.enabled = false;
+        DamageDealer damage = other.gameObject.GetComponent<DamageDealer>();
+        if (damage != null)
+        {
+            currentHealth -= damage.DamageAmount.Value;
+            OnTakeDamage.Invoke();
+        }
+
+        if (currentHealth <= 0.0f)
+        {
+            OnDie.Invoke();
+        }
+    }
+}
+
